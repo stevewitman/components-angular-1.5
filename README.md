9f88456fd756d0f753425109286f8526559957f9
Steve Witman <stevewitman@gmail.com>
2016-05-21 23:42:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[Building Components with Angular 1.5](https://app.pluralsight.com/library/courses/building-components-angular-1-5/table-of-contents)
---
+++ src/ps-movies/movie-list.component.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+
+  var module = angular.module("psMovies");
+
+  module.component("movieList", {
+    template: "Hello from a component"
+  });
+
+}());
