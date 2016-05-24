e0951f9413b2d8a2b0aca28fffcf21732ed34864
Steve Witman <stevewitman@gmail.com>
2016-05-23 23:33:41 -0600

    implement a couple routes using original router

--- src/ps-movies/module.js
+++ src/ps-movies/module.js
@@ -1,5 +1,17 @@
 (function() {
   "use strict";
 
-  angular.module("psMovies", []);
+  var module = angular.module("psMovies", ["ngRoute"]);
+
+  module.config(function($routeProvider) {
+    $routeProvider
+      .when("/list", { template: "<movie-list></movie-list>"})
+      .when("/about", { template: "<app-about></app-about>"})
+      .otherwise({ redirectTo: "/list"});
+  });
+
+  module.component("appAbout", {
+    template: "This is the about page"
+  });
+
 }());
