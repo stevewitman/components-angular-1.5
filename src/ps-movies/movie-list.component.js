7f716c579499b66e380ee004f00813ca63aefe86
Steve Witman <stevewitman@gmail.com>
2016-05-27 19:24:47 -0600

    modified movie-rating-component to remove dependencies and make reusable

--- src/ps-movies/movie-list.component.js
+++ src/ps-movies/movie-list.component.js
@@ -26,17 +26,21 @@
       model.$router.navigate(["Details", {id:id}, "Overview"]);
     };
 
+    model.setRating = function(movie, newRating) {
+      movie.rating = newRating;
+    };
+
     model.upRating = function(movie) {
       if (movie.rating < 5) {
         movie.rating += 1;
       }
-    }
+    };
 
     model.downRating = function(movie) {
       if (movie.rating > 1) {
         movie.rating -= 1;
       }
-    }
+    };
 
   }
 
