c832fe3e39d38d6ddd792e4350d8bba4081d2a0d
Steve Witman <stevewitman@gmail.com>
2016-05-29 00:53:48 -0600

    finish accordian component with simple animation

--- src/ps-movies/accordian.component.js
+++ src/ps-movies/accordian.component.js
@@ -5,13 +5,64 @@
 
   module.component("accordian", {
     transclude: true,
-    template: "<div class='panel-group' ng-transclude></div>"
+    template: "<div class='panel-group' ng-transclude></div>",
+    controller: function() {
+      var model = this;
+      var panels = [];
+
+      model.selectPanel = function(panel) {
+        for (var i in panels) {
+          if (panel === panels[i]) {
+            panels[i].turnOn();
+          } else {
+            panels[i].turnOff();
+          }
+        }
+      }
+
+      model.addPanel = function(panel) {
+        panels.push(panel);
+        if (panels.length > 0) {
+          panels[0].turnOn();
+        }
+      };
+    }
   });
 
   module.component("accordianPanel", {
+    bindings: {
+      "heading": "@"
+    },
+    require: {
+      "parent": "^accordian"
+    },
+    controllerAs: "model",
+    controller: function() {
+      var model = this;
+      model.selected = false;
+
+      model.$onInit = function() {
+        model.parent.addPanel(model);
+      };
+
+      model.turnOn = function() {
+        model.selected = true;
+      };
+
+      model.turnOff = function() {
+        model.selected = false;
+      };
+
+      model.select = function() {
+        model.parent.selectPanel(model);
+      }
+    },
     transclude: true,
     template: "<div class='panel panel-default'>" +
-                "<div class='panel-body' ng-transclude>" +
+                "<div class='panel-heading' ng-click='model.select()'>" +
+                  "<h4 class='panel-title'>{{ model.heading }}</h4>" +
+                "</div>" +
+                "<div ng-if='model.selected' class='panel-body' ng-transclude>" +
                 "</div>" +
               "</div>"
   });
